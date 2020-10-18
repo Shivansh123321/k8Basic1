95098cd139771e8415d592c149bae94178e8ba2d
Shivansh123321 <shivansh123321@gmail.com>
2020-10-18 19:13:55 +0530

    initial commit

---
+++ src/test/java/com/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplicationTests.java
@@ -0,0 +1,11 @@
+package com.in28minutes.rest.webservices.restfulwebservices;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestfulWebServicesApplicationTests {
+
+	
+
+}
